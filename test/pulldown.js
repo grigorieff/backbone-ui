e8105e4561cc0a7d02c498f75415549fdaeefd8b
James Strate <james@getperka.com>
2013-03-28 09:03:41 -0400

    added more tests for pulldown

--- test/pulldown.js
+++ test/pulldown.js
@@ -2,9 +2,103 @@ $(document).ready(function() {
 
   module("Backbone.UI.Pulldown");
 
-  test("triggers the onChange callback", function() {
+  Backbone.UI.setMobile(false);
 
-    Backbone.UI.setMobile(false);
+  test("withoutDataBinding", function(){
+
+    var regions = new Backbone.Collection([{
+      name: 'Americas',
+      notes: 'Bright'
+    }, {
+      name: 'Africa',
+      notes: 'Fruity'
+    }]);
+
+    var pulldown = new Backbone.UI.Pulldown({
+      content: 'test',
+      alternatives: regions,
+      altLabelContent: 'name'
+    }).render();
+
+    $(pulldown.el).find('input').click();
+    $(pulldown._menu.el).find('li a').eq(0).click();
+
+    equal($(pulldown.el).find('.label').text(),'Americas');
+
+
+  });
+
+
+  test("withDataBinding", function(){
+
+    var regions = new Backbone.Collection([{
+      name: 'Americas',
+      notes: 'Bright'
+    }, {
+      name: 'Africa',
+      notes: 'Fruity'
+    }]);
+
+    var coffee = new Backbone.Model({
+      roaster: 'Counter Culture',
+      name: 'Baroida',
+      roastedOn: new Date(2012, 2, 28, 6, 30),
+      acidic: true,
+      region: regions.at(0)
+    });
+
+    var pulldown = new Backbone.UI.Pulldown({
+      model : coffee,
+      content: 'region',
+      alternatives: regions,
+      altLabelContent: 'name'
+    }).render();
+
+    $('body').append(pulldown.el);
+
+    $(pulldown.el).find('input').click();
+    $(pulldown._menu.el).find('li a').eq(1).click();
+
+    equal(coffee.get('region').get('name'),'Africa');
+
+  });
+
+  test("emptyItem", function(){
+
+    var regions = new Backbone.Collection([{
+      name: 'Americas',
+      notes: 'Bright'
+    }, {
+      name: 'Africa',
+      notes: 'Fruity'
+    }]);
+
+    var coffee = new Backbone.Model({
+      roaster: 'Counter Culture',
+      name: 'Baroida',
+      roastedOn: new Date(2012, 2, 28, 6, 30),
+      acidic: true,
+      region: regions.at(0)
+    });
+
+    var pulldown = new Backbone.UI.Pulldown({
+      model : coffee,
+      content: 'region',
+      alternatives: regions,
+      altLabelContent: 'name',
+      emptyItem: true
+    }).render();
+
+    $('body').append(pulldown.el);
+
+    $(pulldown.el).find('input').click();
+    $(pulldown._menu.el).find('li a').eq(0).click();
+
+    equal(coffee.get('region'),null);
+
+  });
+
+  test("triggers the onChange callback", function() {
 
     var regions = new Backbone.Collection([{
       name: 'Americas',
