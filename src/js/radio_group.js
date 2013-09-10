c9a1ffa1bb121e0ae5b3e8ca8be107b26b62f1a9
Jack Carrig <jack@getperka.com>
2013-09-10 17:26:42 -0400

    being converting radio group to native elements

--- src/js/radio_group.js
+++ src/js/radio_group.js
@@ -3,7 +3,10 @@
 
     options : {
       // A callback to invoke with the selected item whenever the selection changes
-      onChange : Backbone.UI.noop
+      onChange : Backbone.UI.noop,
+      // The property of the model describing the label that 
+      // should be placed describing the entire radio group
+      labelContent : null
     },
 
     initialize : function() {
@@ -17,7 +20,7 @@
     },
 
     // public accessors
-    selectedItem : null,
+    //selectedItem : null,
 
     render : function() {
 
@@ -28,57 +31,66 @@
 
       this.selectedItem = this._determineSelectedItem() || this.selectedItem;
 
-      this._ul = $.el.ul();
+      this.groupLabel = $.el.label();
+      
+      //var groupLabelText = this.resolveContent(this.model, this.options.labelContent) || this.options.labelContent;
+      //this.groupLabel.appendChild($.el.span(groupLabelText));
+      
       var selectedValue = this._valueForItem(this.selectedItem);
       _(this._collectionArray()).each(function(item) {
 
-        var selected = selectedValue === this._valueForItem(item);
-
+        var val = this._valueForItem(item);
+        var selected = selectedValue === val;
         var label = this.resolveContent(item, this.options.altLabelContent);
-        if(label.nodeType === 1) {
-          $('a',label).click(function(e){
-            e.stopPropagation(); 
-          });
-        }
         
-        var li = $.el.li(
-          $.el.a({className : 'choice' + (selected ? ' selected' : '')},
-            $.el.div({className : 'mark' + (selected ? ' selected' : '')}, 
-              selected ? '\u25cf' : '\u00a0')));      
+        // if(label.nodeType === 1) {
+        //           $('a',label).click(function(e){
+        //             e.stopPropagation(); 
+        //           });
+        //         }
+        
+        var input = $.el.input();
+        $(input).attr({ 
+          type : 'radio',
+          name : this.options.content,
+          value : val,
+          checked : selected
+        });
         
         // insert label into li then add to ul
-        $.el.div({className : 'label'}, label).appendTo(li);
-        this._ul.appendChild(li);
+        //$.el.div({className : 'label'}, label).appendTo(li);
+        this.groupLabel.appendChild($.el.label(input, label));
 
-        $(li).bind('click', _.bind(this._onChange, this, item));
+        //$(li).bind('click', _.bind(this._onChange, this, item));
         
       }, this);
-      this.el.appendChild(this._ul);
-      this._updateClassNames();
+     
+      this.el.appendChild(this.groupLabel);
+      //this._updateClassNames();
       return this;
-    },
-
-    _onChange : function(item) {
-      //check if item selected actually changed
-      var changed = this.selectedItem !== item;
-      this._setSelectedItem(item);
-      this.render();
-
-      if(_(this.options.onChange).isFunction() && changed) this.options.onChange(item);
-      return false;
-    },
-    
-    _updateClassNames : function() {
-      var children = this._ul.childNodes;
-      if(children.length > 0) {
-        _(children).each(function(child, index) {
-          $(child).removeClass('first');
-          $(child).removeClass('last');
-          $(child).addClass(index % 2 === 0 ? 'even' : 'odd');
-        });
-        $(children[0]).addClass('first');
-        $(children[children.length - 1]).addClass('last');
-      }
     }
+
+    // _onChange : function(item) {
+    //       //check if item selected actually changed
+    //       var changed = this.selectedItem !== item;
+    //       this._setSelectedItem(item);
+    //       this.render();
+    // 
+    //       if(_(this.options.onChange).isFunction() && changed) this.options.onChange(item);
+    //       return false;
+    //     },
+    
+    // _updateClassNames : function() {
+    //      var children = this._ul.childNodes;
+    //      if(children.length > 0) {
+    //        _(children).each(function(child, index) {
+    //          $(child).removeClass('first');
+    //          $(child).removeClass('last');
+    //          $(child).addClass(index % 2 === 0 ? 'even' : 'odd');
+    //        });
+    //        $(children[0]).addClass('first');
+    //        $(children[children.length - 1]).addClass('last');
+    //      }
+    //    }
   });
 }());
