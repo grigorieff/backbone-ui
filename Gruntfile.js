c8dc2773abe9dc008fe5b07ac3d3d992472d01dd
Scott Feeney <scott@getperka.com>
2014-03-07 14:03:05 -0500

    Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
-module.exports = function (grunt) {
-	grunt.loadNpmTasks('grunt-contrib-qunit');
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-qunit');
 
-	grunt.registerTask('test', 'qunit');
+  grunt.registerTask('test', 'qunit');
 
-	grunt.initConfig({
-		qunit: {
-			files: ['test/test.html']
-		}
-	});
+  grunt.initConfig({
+    qunit: {
+      files: ['test/test.html']
+    }
+  });
 };
