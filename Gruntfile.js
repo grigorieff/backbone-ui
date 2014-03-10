654b78aeb46ae3205ddc26dac0fadcf70f0161a0
Scott Feeney <scott@getperka.com>
2014-03-10 16:07:49 -0400

    Make 'grunt build' and 'grunt test' work
    Still to do: generate backbone-ui.css, backbone-ui-min.{css,js},
    backbone-ui{-min}.tar.gz.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,43 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-
-  grunt.registerTask('test', 'qunit');
-
   grunt.initConfig({
     qunit: {
       files: ['test/test.html']
+    },
+
+    browserify: {
+      basic: {
+        src: ['index.js'],
+        dest: 'dist/backbone-ui.js',
+        options: {
+          external: ['underscore', 'backbone'],
+          alias: ['./index.js:backbone-ui']
+        }
+      }
+    },
+
+    umd: {
+      default: {
+        src: 'dist/backbone-ui.js',
+        template: './umd-template.hbs',
+        objectToExport: "require('backbone-ui')",
+        globalAlias: 'Backbone.UI',
+        deps: {
+          'default': ['_', 'Backbone'],
+          amd: ['underscore', 'backbone'],
+          cjs: ['underscore', 'backbone'],
+          global: ['_', 'Backbone']
+        },
+        browserifyMapping: '{"backbone":Backbone,"underscore":_}'
+      }
     }
   });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-umd');
+
+  grunt.registerTask('build', ['browserify', 'umd']);
+  grunt.registerTask('default', ['build']);
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.registerTask('test', ['build', 'qunit']);
 };
